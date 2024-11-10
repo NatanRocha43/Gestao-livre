4aec9d2e7048f6951d38a5a6d25420ddc6fcba30
NatanRocha43 <natan.paularocha@gmail.com>
2024-11-10 18:31:30 -0300

    feat: ensure only premium users can generate ai report

--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -17,12 +17,14 @@ import { generateAiReport } from "../_actions/generate-ai-report";
 import { useState } from "react";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
 import remarkGfm from "remark-gfm";
+import Link from "next/link";
 
 interface AiReportButtonProps {
+  hasPremiumPlan: boolean;
   month: string;
 }
 
-const AiReportButton = ({ month }: AiReportButtonProps) => {
+const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
   const [reportIsLoading, setReportIsLoading] = useState(false);
   const [report, setReport] = useState<string | null>(null);
   const handleGenerateReportClick = async () => {
@@ -48,33 +50,59 @@ const AiReportButton = ({ month }: AiReportButtonProps) => {
           </Button>
         </DialogTrigger>
         <DialogContent className="max-w-[600px]">
-          <DialogHeader>
-            <DialogTitle>Relatório com IA</DialogTitle>
-            <DialogDescription>
-              Use inteligência artificial para gerar um relatório com insights
-              sobre suas finanças.
-            </DialogDescription>
-          </DialogHeader>
-          {report && (
-            <ScrollArea className="prose prose-slate max-h-[450px] text-white marker:text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
-              <Markdown remarkPlugins={[remarkGfm]}>{report}</Markdown>
-            </ScrollArea>
+          {hasPremiumPlan ? (
+            <>
+              <DialogHeader>
+                <DialogTitle>Relatório com IA</DialogTitle>
+                <DialogDescription>
+                  Use inteligência artificial para gerar um relatório com
+                  insights sobre suas finanças.
+                </DialogDescription>
+              </DialogHeader>
+              {report && (
+                <ScrollArea className="prose prose-slate max-h-[450px] text-white marker:text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+                  <Markdown remarkPlugins={[remarkGfm]}>{report}</Markdown>
+                </ScrollArea>
+              )}
+              <DialogFooter>
+                <DialogClose>
+                  <Button variant="ghost" className="font-bold">
+                    Cancelar
+                  </Button>
+                </DialogClose>
+                <Button
+                  onClick={handleGenerateReportClick}
+                  disabled={reportIsLoading}
+                  className="font-bold"
+                >
+                  {reportIsLoading && (
+                    <Loader2Icon className="mr-1 animate-spin" />
+                  )}
+                  {reportIsLoading ? "Gerando relatório..." : "Gerar relatório"}
+                </Button>
+              </DialogFooter>
+            </>
+          ) : (
+            <>
+              <DialogHeader>
+                <DialogTitle>Relatório com IA</DialogTitle>
+                <DialogDescription>
+                  Para gerar um relatório com inteligência artificial, voce
+                  precisa ter uma assinatura premium.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose>
+                  <Button variant="ghost" className="font-bold">
+                    Cancelar
+                  </Button>
+                </DialogClose>
+                <Button asChild>
+                  <Link href="/subscription">Assinar premium</Link>
+                </Button>
+              </DialogFooter>
+            </>
           )}
-          <DialogFooter>
-            <DialogClose>
-              <Button variant="ghost" className="font-bold">
-                Cancelar
-              </Button>
-            </DialogClose>
-            <Button
-              onClick={handleGenerateReportClick}
-              disabled={reportIsLoading}
-              className="font-bold"
-            >
-              {reportIsLoading && <Loader2Icon className="mr-1 animate-spin" />}
-              {reportIsLoading ? "Gerando relatório..." : "Gerar relatório"}
-            </Button>
-          </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
