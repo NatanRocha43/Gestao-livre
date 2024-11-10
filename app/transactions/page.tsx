d5813fb96d0dabd048a29632791ffb20ddc2c403
NatanRocha43 <natan.paularocha@gmail.com>
2024-11-10 07:57:15 -0300

    fix: Adjusting the scroll of transactions

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -20,16 +20,18 @@ const TransactionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 overflow-hidden p-6">
-        {/* TÍTULO E BOTÃO */}
-        <div className="flex w-full items-center justify-between">
-          <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton />
-        </div>
-        <ScrollArea>
+      <ScrollArea>
+        <div className="space-y-6 overflow-hidden p-6">
+          {/* TÍTULO E BOTÃO */}
+
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-2xl font-bold">Transações</h1>
+            <AddTransactionButton />
+          </div>
+
           <DataTable columns={transactionColumns} data={transactions} />
-        </ScrollArea>
-      </div>
+        </div>
+      </ScrollArea>
     </>
   );
 };
